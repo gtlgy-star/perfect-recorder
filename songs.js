bbcbaca0582af47856b22145996d09f030daf247
gtlgy-star <toughguy1234321@gmail.com>
2026-05-12 14:57:39 +0900

    v5.12 조표 삽입

---
+++ songs.js
@@ -0,0 +1,98 @@
+const LOCAL_SHORT_SONGS = [
+  {
+    songId: "HAKGYOJONG",
+    title: "\uD559\uAD50\uC885",
+    bpm: 84,
+    timeSignature: "4/4",
+    recorderImage: "",
+    notation: "G5/1 G5/1 A5/1 A5/1 G5/1 G5/1 E5/2 G5/1 G5/1 E5/1 E5/1 D5/3 R/1 G5/1 G5/1 A5/1 A5/1 G5/1 G5/1 E5/2 G5/1 E5/1 D5/1 E5/1 C5/3 R/1"
+  },
+  {
+    songId: "SAME_SAME",
+    title: "\uB611\uAC19\uC544\uC694",
+    bpm: 90,
+    timeSignature: "3/4",
+    recorderImage: "",
+    notation: "C5/1 E5/1 G5/1 C5/1 E5/1 G5/1 A5/1 A5/1 A5/1 G5/2 R/1 F5/1 F5/1 F5/1 E5/1 E5/1 E5/1 D5/1 D5/1 D5/1 C5/2 R/1"
+  },
+  {
+    songId: "ODE",
+    title: "\uD658\uD76C\uC758 \uC1A1\uAC00",
+    bpm: 92,
+    timeSignature: "4/4",
+    recorderImage: "",
+    notation: "E5/1 E5/1 F5/1 G5/1 G5/1 F5/1 E5/1 D5/1 C5/1 C5/1 D5/1 E5/1 E5/1 D5/1 D5/2 E5/1 E5/1 F5/1 G5/1 G5/1 F5/1 E5/1 D5/1 C5/1 C5/1 D5/1 E5/1 D5/1 C5/1 C5/2"
+  },
+  {
+    songId: "JAGEUNBYEOL",
+    title: "\uC791\uC740\uBCC4",
+    bpm: 90,
+    timeSignature: "4/4",
+    recorderImage: "",
+    notation: "C5/1 C5/1 G5/1 G5/1 A5/1 A5/1 G5/2 F5/1 F5/1 E5/1 E5/1 D5/1 D5/1 C5/2 G5/1 G5/1 F5/1 F5/1 E5/1 E5/1 D5/2 G5/1 G5/1 F5/1 F5/1 E5/1 E5/1 D5/2 C5/1 C5/1 G5/1 G5/1 A5/1 A5/1 G5/2 F5/1 F5/1 E5/1 E5/1 D5/1 D5/1 C5/2"
+  },
+  {
+    songId: "NABIYA",
+    title: "\uB098\uBE44\uC57C",
+    bpm: 88,
+    timeSignature: "4/4",
+    recorderImage: "",
+    notation: "G5/1 E5/1 E5/2 F5/1 D5/1 D5/2 C5/1 D5/1 E5/1 F5/1 G5/1 G5/1 G5/2 G5/1 E5/1 E5/1 E5/1 F5/1 D5/1 D5/2 C5/1 E5/1 G5/1 G5/1 E5/1 E5/1 E5/2 D5/1 D5/1 D5/1 D5/1 D5/1 E5/1 F5/2 E5/1 E5/1 E5/1 E5/1 E5/1 F5/1 G5/2 G5/1 E5/1 E5/2 F5/1 D5/1 D5/2 C5/1 E5/1 G5/1 G5/1 E5/1 E5/1 E5/2"
+  },
+  {
+    songId: "TONGTONG",
+    title: "\uD1B5\uD1B5\uD1B5\uD1B5",
+    bpm: 90,
+    timeSignature: "4/4",
+    recorderImage: "",
+    notation: "C5/1 C5/1 C5/1 C5/1 C5/0.5 D5/0.5 E5/0.5 D5/0.5 C5/1 R/1 D5/1 D5/1 D5/1 D5/1 D5/0.5 E5/0.5 F5/0.5 E5/0.5 D5/1 R/1 E5/1 E5/1 E5/1 E5/1 E5/0.5 F5/0.5 G5/0.5 F5/0.5 E5/1 R/1 F5/1 F5/1 F5/1 F5/1 F5/0.5 E5/0.5 D5/0.5 E5/0.5 F5/1 R/1 G5/1 G5/1 G5/1 G5/1 G5/0.5 F5/0.5 E5/0.5 F5/0.5 G5/1 R/1 G5/0.5 F5/0.5 E5/0.5 F5/0.5 G5/0.5 F5/0.5 E5/0.5 F5/0.5 G5/0.5 F5/0.5 E5/0.5 D5/0.5 C5/1 R/1"
+  },
+  {
+    songId: "DRAGONFLY",
+    title: "\uC7A0\uC790\uB9AC",
+    bpm: 90,
+    timeSignature: "4/4",
+    recorderImage: "",
+    notation: "E5/1 C5/0.5 D5/0.5 E5/1 G5/1 D5/1 D5/1 D5/1 R/1 E5/1 C5/0.5 D5/0.5 E5/1 G5/1 A5/1 A5/1 A5/2 C6/2 B5/2 A5/2 G5/2 C6/2 B5/2 A5/2 G5/2 E5/1 C5/0.5 D5/0.5 E5/1 G5/1 D5/1 D5/1 D5/2 E5/1 C5/0.5 D5/0.5 E5/1 G5/1 A5/1 B5/1 C6/2 R/2"
+  },
+  {
+    songId: "HOMETOWN_SPRING",
+    title: "\uACE0\uD5A5\uC758 \uBD04",
+    bpm: 90,
+    timeSignature: "4/4",
+    recorderImage: "",
+    notation: "G5/1 G5/1 E5/0.5 F5/0.5 G5/1 A5/1 A5/1 G5/2 G5/1 C6/1 E6/1 D6/0.5 C6/0.5 D6/3 R/1 E6/1 E6/1 D6/1 D6/1 C6/1 D6/0.5 C6/0.5 A5/2 G5/1 G5/1 G5/1 E5/0.5 D5/0.5 C5/3 R/1 D5/1 D5/1 E5/1 C5/1 D5/2 E5/1 G5/1 A5/1 C6/1 E6/1 D6/0.5 C6/0.5 D6/3 R/1 E6/1 E6/1 D6/1 D6/1 C6/1 D6/0.5 C6/0.5 A5/1 A5/1 G5/1 G5/1 G5/1 E5/0.5 D5/0.5 C5/3 R/1"
+  }
+];
+
+function getLocalShortSongs() {
+  return LOCAL_SHORT_SONGS.map(song => ({ ...song }));
+}
+
+const TEXTBOOK_SONGS_BY_GRADE = {
+  "3": [
+    { songId: "TB3_CUCKOO", title: "뻐꾸기", bpm: 84, timeSignature: "3/4", notation: "D6/1 B5/1 R/1 D6/1 B5/1 R/1 A5/1 G5/1 A5/1 G5/2 R/1 A5/1 A5/1 B5/1 C6/2 A5/1 B5/1 B5/1 C6/1 D6/2 B5/1 D6/2 B5/1 D6/2 B5/1 C6/1 B5/1 A5/1 G5/2 R/1" },
+    { songId: "TB3_MORNING", title: "아침", bpm: 88, timeSignature: "3/4", notation: "B5/1 B5/1 A5/1 G5/2 R/1 B5/1 B5/1 A5/1 G5/2 R/1 B5/1 C6/1 D6/1 D6/1 C6/1 B5/1 C6/1 A5/1 B5/1 C6/1 C6/1 B5/1 A5/1 G5/2 R/1 B5/1 B5/1 C6/1 D6/2 R/1 B5/1 B5/1 A5/1 G5/2 R/1" },
+    { songId: "TB3_RAIN", title: "구슬비", bpm: 88, timeSignature: "2/4", notation: "E5/0.5 G5/0.5 G5/0.5 F5/0.5 E5/0.5 G5/0.5 G5/0.5 F5/0.5 E5/1 G5/1 F5/1.5 R/0.5 D5/0.5 F5/0.5 F5/0.5 E5/0.5 D5/0.5 F5/0.5 F5/0.5 E5/0.5 D5/1 F5/1 E5/1.5 R/0.5 C5/0.5 C5/0.5 E5/0.5 G5/0.5 C6/1 A5/1 G5/0.5 G5/0.5 A5/1 G5/1.5 R/0.5 E5/0.5 G5/0.5 G5/0.5 F5/0.5 E5/0.5 G5/0.5 G5/0.5 F5/0.5 E5/1 D5/1 C5/1.5 R/0.5" },
+    { songId: "TB3_PLANE", title: "비행기", bpm: 96, timeSignature: "4/4", notation: "B5/1 A5/1 G5/1 A5/1 B5/1 B5/1 B5/2 A5/1 A5/1 A5/2 B5/1 B5/1 B5/2 B5/1 A5/1 G5/1 A5/1 B5/1 B5/1 B5/2 A5/1 A5/1 B5/1 A5/1 G5/2" },
+    { songId: "TB3_AUTUMN", title: "가을길", bpm: 100, timeSignature: "4/4", notation: "E5/1 E5/0.5 E5/0.5 E5/1 F5/0.5 G5/0.5 A5/1 G5/0.5 F5/0.5 E5/1 R/1 G5/1 G5/0.5 G5/0.5 G5/1 A5/0.5 B5/0.5 C6/1 B5/0.5 A5/0.5 G5/1 R/1 E5/1 E5/0.5 E5/0.5 E5/1 F5/0.5 G5/0.5 A5/1 G5/0.5 F5/0.5 E5/1 R/1 A5/1 C6/1 B5/1 D6/1 C6/1 C6/1 C6/1 R/1 A5/1 C6/0.5 C6/0.5 C6/1 R/1 A5/1 C6/0.5 C6/0.5 C6/1 R/1 A5/1 C6/0.5 C6/0.5 C6/1 C6/1 C6/1 B5/0.5 A5/0.5 G5/1 F5/1 E5/1 E5/0.5 E5/0.5 E5/1 F5/0.5 G5/0.5 A5/1 G5/0.5 F5/0.5 E5/1 R/1 A5/1 C6/1 B5/1 D6/1 C6/1 C6/1 C6/1 R/1" }
+  ],
+  "4": [
+    { songId: "TB4_RIVER", title: "시냇물", bpm: 86, timeSignature: "4/4", notation: "E5/1 F5/1 G5/1 A5/1 G5/1 F5/1 E5/2" },
+    { songId: "TB4_TRAVEL", title: "즐거운 여행", bpm: 96, timeSignature: "4/4", notation: "C5/1 D5/1 E5/1 G5/1 A5/1 G5/1 E5/2" },
+    { songId: "TB4_FRIEND", title: "우리 친구", bpm: 90, timeSignature: "4/4", notation: "G5/1 E5/1 F5/1 G5/1 A5/1 G5/1 D5/2" },
+    { songId: "TB4_PUFF_JACKY", title: "퍼프와 재키", bpm: 90, timeSignature: "4/4", notation: "G5/1 A5/1 G5/1 E5/1 D5/1 E5/1 G5/2" }
+  ],
+  "5": [
+    { songId: "TB5_FOREST", title: "숲속 이야기", bpm: 88, timeSignature: "3/4", notation: "E5/1 G5/1 A5/1 G5/1 E5/1 D5/1" },
+    { songId: "TB5_DREAM", title: "꿈을 향해", bpm: 94, timeSignature: "4/4", notation: "C5/1 E5/1 G5/1 C6/1 B5/1 G5/1 E5/2" },
+    { songId: "TB5_SUMMER", title: "여름 바다", bpm: 98, timeSignature: "4/4", notation: "G5/1 A5/1 B5/1 A5/1 G5/1 E5/1 D5/2" }
+  ],
+  "6": [
+    { songId: "TB6_MORNING", title: "아침 햇살", bpm: 90, timeSignature: "4/4", notation: "C5/1 D5/1 G5/1 A5/1 G5/1 E5/1 C5/2" },
+    { songId: "TB6_WORLD", title: "넓은 세상", bpm: 100, timeSignature: "4/4", notation: "E5/1 G5/1 A5/1 C6/1 B5/1 A5/1 G5/2" },
+    { songId: "TB6_HOPE", title: "희망의 노래", bpm: 92, timeSignature: "4/4", notation: "D5/1 F5/1 A5/1 B5/1 A5/1 F5/1 D5/2" }
+  ]
+};
+
+const BONUS_GROWTH_SONGS = [];
